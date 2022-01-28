31feb7375bb11ec5fc3b33ef14d8816e15a34e93
Diganta Chaudhuri <digantachaudhuri03@gmail.com>
2022-01-28 12:19:21 +0530

    DONE state-management

--- second-project/my-app/src/App.js
+++ second-project/my-app/src/App.js
@@ -1,23 +1,46 @@
-// import logo from "./Assets/logo.svg";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  // const [name, setName] = useState("Diganta");
+  const [show, setShow] = useState(true);
+  const [events, setEvents] = useState([
+    { title: "This is Title No 1 Nothing.........", id: 1 },
+    { title: "This is Title No 2 And This is jubriish", id: 2 },
+    { title: "This is Title No 3 And We are going to Kolkata", id: 3 },
+  ]);
+  // Best Practice to change state in an array
+  console.log(show);
+  const handleClick = (id) => {
+    setEvents((prevEvents) => {
+      return prevEvents.filter((item) => {
+        return item.id !== id;
+      });
+    });
+    console.log(id);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="../public/logo.svg" className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!show && (
+        <div>
+          <button onClick={() => setShow(true)}>show Events</button>
+        </div>
+      )}
+      {show && (
+        <div>
+          <button onClick={() => setShow(false)}>Hide Events</button>
+        </div>
+      )}
+      {show
+        ? events.map((item, idx) => (
+            <div key={item.id}>
+              <h2>
+                {idx} -- {item.title}
+              </h2>
+              <button onClick={() => handleClick(item.id)}>Delete Event</button>
+            </div>
+          ))
+        : ""}
     </div>
   );
 }
